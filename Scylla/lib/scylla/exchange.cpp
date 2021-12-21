add808cf3925721d57228e98b58185257cd78288
analyman <beam.analyman@gmail.com>
2021-12-22 01:01:34 +0800

    binary search

--- Scylla/lib/scylla/exchange.cpp
+++ Scylla/lib/scylla/exchange.cpp
@@ -1,4 +1,5 @@
 #include "scylla/exchange.h"
+#include <algorithm>
 
 static char char_tolower(char c) {
     if (c >= 'A' && c <= 'Z') {
@@ -11,6 +12,22 @@ static int strcmp_nocase(const char* a, const char* b) {
     for (;*a && *b; ++a, ++b) {
         char c1 = char_tolower(*a);
         char c2 = char_tolower(*b);
+
+        if (c1 > c2) {
+            return 1;
+        } else if (c1 < c2) {
+            return -1;
+        }
+    }
+
+    if (*a) return  1;
+    if (*b) return -1;
+    return 0;
+}
+static int strcmp_case(const char* a, const char* b) {
+    for (;*a && *b; ++a, ++b) {
+        char c1 = *a;
+        char c2 = *b;
 
         if (c1 > c2) {
             return 1;
@@ -57,11 +74,13 @@ void* ExchangeData::lookup_trampoline(const char* dllname, const char* funcname)
 }
 
 const char* ExchangeData::lookup_key(const char* key) {
-    for(size_t i=0;i<m_numOfKV;i++) {
-        if(strcmp_nocase(m_key_value_str[i]->key, key) == 0) {
-            return m_key_value_str[i]->value;
-        }
-    }
+    auto begin = this->m_key_value_str;
+    auto end = this->m_key_value_str + this->m_numOfKV;
+    auto found = std::lower_bound(begin, end, key, [](const StringPair* a, const char* b) {
+        return strcmp_case(a->key, b) < 0;
+    });
 
-    return nullptr;
+    if (found == end || strcmp_case((*found)->key, key) != 0)
+        return nullptr;
+    return (*found)->value;
 }

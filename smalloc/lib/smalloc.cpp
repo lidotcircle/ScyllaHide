16cf8cdfac0462fb0b22be0a56b1f58da670117a
analyman <beam.analyman@gmail.com>
2022-01-26 11:34:51 +0800

    thread safe memory allocation and improvement

--- smalloc/lib/smalloc.cpp
+++ smalloc/lib/smalloc.cpp
@@ -1,4 +1,5 @@
 #include "smalloc.h"
+#include "utils.hpp"
 #include <Windows.h>
 
 #define __abort(msg) \
@@ -31,6 +32,7 @@ struct slot_header {
 #ifdef SMALLOC_DEBUG
     size_t slot_magic;
 #endif
+    slot_header* m_prev;
     size_t m_size;
     page_header* m_page;
     bool   m_free;
@@ -84,6 +86,7 @@ static page_header* alloc_page(size_t size)
 #ifdef SMALLOC_DEBUG
     first_slot->slot_magic = SLOT_MAGIC;
 #endif
+    first_slot->m_prev = NULL;
     first_slot->m_free = true;
     first_slot->m_page = page;
     first_slot->m_size = nsize - sizeof(page_header);
@@ -102,11 +105,19 @@ static void adjust_slot(slot_header* sh, size_t size) {
 #ifdef SMALLOC_DEBUG
     nsh->slot_magic = sh->slot_magic;
 #endif
+    nsh->m_prev = sh;
     nsh->m_free = true;
     nsh->m_page = sh->m_page;
     nsh->m_size = sh->m_size - size;
 
     sh->m_size = size;
+
+    auto _nn = reinterpret_cast<size_t>(nsh) + nsh->m_size;
+    auto page_end = sh->m_page->m_size + reinterpret_cast<size_t>(sh->m_page);
+    if (_nn < page_end) {
+        auto nnsh = reinterpret_cast<slot_header*>(_nn);
+        nnsh->m_prev = nsh;
+    }
 }
 
 static void* alloc_slot_from_page(page_header* page, size_t size)
@@ -156,19 +167,50 @@ static void* alloc_slot(size_t size)
 static void merge_slot(slot_header* sh)
 {
     auto _n = reinterpret_cast<size_t>(sh) + sh->m_size;
-    if (_n < sh->m_page->m_size + reinterpret_cast<size_t>(sh->m_page)) {
+    auto page_end = sh->m_page->m_size + reinterpret_cast<size_t>(sh->m_page);
+    if (_n < page_end) {
         auto nsh = reinterpret_cast<slot_header*>(_n);
 #ifdef SMALLOC_DEBUG
         if (nsh->slot_magic != SLOT_MAGIC)
             __abort("slot magic error");
 #endif
 
-        if (nsh->m_free)
+        if (nsh->m_free) {
             sh->m_size += nsh->m_size;
+
+            auto _nn = reinterpret_cast<size_t>(nsh) + nsh->m_size;
+            if (_nn < page_end) {
+                auto nnsh = reinterpret_cast<slot_header*>(_nn);
+                nnsh->m_prev = sh;
+            }
+        }
+    }
+
+    if (sh->m_prev && sh->m_prev->m_free) {
+        auto prev = sh->m_prev;
+        prev->m_size += sh->m_size;
+
+        _n = reinterpret_cast<size_t>(sh) + sh->m_size;
+        if (_n < page_end) {
+            auto nsh = reinterpret_cast<slot_header*>(_n);
+            nsh->m_prev = prev;
+        }
     }
 }
 
+static CRITICAL_SECTION* g_cs_alloc = nullptr;
+static char cs_mem[sizeof(CRITICAL_SECTION)];
+#define MEM_LOCK() \
+    if (g_cs_alloc == nullptr) { \
+        g_cs_alloc = (CRITICAL_SECTION*)cs_mem; \
+        InitializeCriticalSection(g_cs_alloc); \
+    } \
+    EnterCriticalSection(g_cs_alloc); \
+    auto d1 = defer([&]() { LeaveCriticalSection(g_cs_alloc); })
+
 extern "C" void* smalloc(size_t size) {
+    MEM_LOCK();
+
     auto ptr = alloc_slot(size);
     if (ptr == nullptr)
         __abort("smalloc failed");
@@ -176,11 +218,15 @@ extern "C" void* smalloc(size_t size) {
 }
 
 extern "C" void* smalloc_aligned(size_t size, size_t alignment) {
+    MEM_LOCK();
+
     __abort("not implemented");
     return nullptr;
 }
 
 extern "C" void  sfree(void* ptr) {
+    MEM_LOCK();
+
     auto sh = reinterpret_cast<slot_header*>(reinterpret_cast<size_t>(ptr) - sizeof(slot_header));
 #ifdef SMALLOC_DEBUG
     if (sh->slot_magic != SLOT_MAGIC)
